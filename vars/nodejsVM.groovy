61e59f39e6bbccfbdf74f72d2712c0228d493b55
User <kalidindi.moulika@gmail.com>
2024-02-14 20:30:17 -0600

    jenkins-apply

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -94,7 +94,7 @@ def call(Map configMap) {
                         string(name: 'environment', value: "dev")
                     ]
 
-                        build job: "../${configMap.component}-deploy", wait: true ,parameters: params
+                        build job: "../${configMap.component}-deloy", wait: true ,parameters: params
 
                 }
               }
